f22a06e754ac2db9c64a72cb43f58751e3136f95
YashKr01 <yashkumar201301@gmail.com>
2022-03-22 22:12:32 +0530

    -> API Keys -> ApiInterface.kt changes

--- app/src/main/java/com/example/foodium/data/network/ApiInterface.kt
+++ app/src/main/java/com/example/foodium/data/network/ApiInterface.kt
@@ -2,6 +2,9 @@ package com.example.foodium.data.network
 
 import com.example.foodium.data.network.model.FoodJoke
 import com.example.foodium.data.network.model.RecipesResponse
+import com.example.foodium.utils.Constants.API_KEY1
+import com.example.foodium.utils.Constants.API_KEY2
+import com.example.foodium.utils.Constants.API_KEY3
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,17 +12,17 @@ import retrofit2.http.QueryMap
 
 interface ApiInterface {
 
-    @GET("/recipes/complexSearch")
+    @GET("/recipes/complexSearch?apiKey=$API_KEY1")
     suspend fun getRecipes(
         @QueryMap queries: Map<String, String>
     ): Response<RecipesResponse>
 
-    @GET("/recipes/complexSearch")
+    @GET("/recipes/complexSearch?apiKey=$API_KEY2")
     suspend fun searchRecipes(
         @QueryMap searchQuery: Map<String, String>
     ): Response<RecipesResponse>
 
-    @GET("food/jokes/random")
+    @GET("food/jokes/random?apiKey=$API_KEY3")
     suspend fun getFoodJoke(
         @Query("apiKey") apiKey: String
     ): Response<FoodJoke>
