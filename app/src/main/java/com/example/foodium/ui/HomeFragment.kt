ee40bf29994ebcbed2db6a024a4ec20d6eaa0a22
YashKr01 <yashkumar201301@gmail.com>
2022-03-23 11:34:50 +0530

    -> HomeFragment.kt structured

--- app/src/main/java/com/example/foodium/ui/HomeFragment.kt
+++ app/src/main/java/com/example/foodium/ui/HomeFragment.kt
@@ -1,16 +1,24 @@
 package com.example.foodium.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.foodium.databinding.FragmentHomeBinding
+import com.example.foodium.utils.Resource
+import com.example.foodium.viewmodel.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +26,26 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.recipesList.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is Resource.Loading -> {
+                    Log.d("STATUS", "onViewCreated: LOADING")
+                }
+                is Resource.Error -> {
+                    Log.d("STATUS", "onViewCreated: ERROR")
+                }
+                is Resource.Success -> {
+                    Log.d("STATUS", "onViewCreated: SUCCESS")
+                    Log.d("STATUS", "onViewCreated: ${result.data}")
+                }
+            }
+        }
+
     }
 
     override fun onDestroyView() {
