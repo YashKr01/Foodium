f7bf658a68e3602d3002244dd76d46b5f9a70aa6
YashKr01 <yashkumar201301@gmail.com>
2022-05-08 22:40:03 +0530

    -> Recipe Details Activity 1. Menu with functionality 2. Desc, Link, Chips etc

--- app/src/main/java/com/example/foodium/ui/activities/RecipeDetailsActivity.kt
+++ app/src/main/java/com/example/foodium/ui/activities/RecipeDetailsActivity.kt
@@ -1,7 +1,13 @@
 package com.example.foodium.ui.activities
 
+import android.content.Intent
+import android.graphics.Paint
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.core.view.isVisible
 import androidx.navigation.navArgs
 import com.bumptech.glide.Glide
@@ -9,14 +15,17 @@ import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.foodium.R
 import com.example.foodium.data.database.model.RecipeEntity
 import com.example.foodium.databinding.ActivityRecipeDetailsBinding
-import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipDrawable
+import com.example.foodium.viewmodel.RecipeDetailsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RecipeDetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRecipeDetailsBinding
 
     private val recipeEntity by navArgs<RecipeDetailsActivityArgs>()
+
+    private val viewModel by viewModels<RecipeDetailsViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +35,10 @@ class RecipeDetailsActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         updateUI(recipeEntity.recipe)
+
+        binding.textLink.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(recipeEntity.recipe.sourceUrl)))
+        }
 
     }
 
@@ -47,16 +60,72 @@ class RecipeDetailsActivity : AppCompatActivity() {
 
             // chips
             chipVegan.isVisible = recipeEntity.vegan
-            chipDairyFree.isVisible=recipeEntity.dairyFree
+            chipDairyFree.isVisible = recipeEntity.dairyFree
             chipGlutenFree.isVisible = recipeEntity.glutenFree
             chipPopular.isVisible = recipeEntity.popular
             chipHealthy.isVisible = recipeEntity.veryHealthy
             chipVegetarian.isVisible = recipeEntity.vegetarian
 
+            // rating bar
             ratingBar.rating = (recipeEntity.healthScore / 10).toFloat()
+
+            // link
+            textLink.text = recipeEntity.sourceUrl
+            textLink.paintFlags = textLink.paintFlags or Paint.UNDERLINE_TEXT_FLAG
 
         }
 
     }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        try {
+            if (recipeEntity.recipe.saved) {
+                val icon = menu?.findItem(R.id.menu_save)
+                icon?.let { changeMenuIcon(it, R.drawable.ic_favorite_solid) }
+            }
+        } catch (e: Exception) {
+
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.recipe_details_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+
+            // share RecipeURL
+            R.id.menu_share -> {
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                shareIntent.apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_SUBJECT, "Recipe URL")
+                    putExtra(Intent.EXTRA_TEXT, recipeEntity.recipe.sourceUrl)
+                    startActivity(this)
+                }
+                true
+            }
+
+            // Insert/Delete Recipe
+            else -> {
+
+                if (recipeEntity.recipe.saved) {
+                    viewModel.deleteRecipe(recipeEntity.recipe)
+                    recipeEntity.recipe.saved = false
+                    changeMenuIcon(item, R.drawable.ic_favorite_hollow)
+                } else {
+                    viewModel.insertRecipe(recipeEntity.recipe)
+                    recipeEntity.recipe.saved = true
+                    changeMenuIcon(item, R.drawable.ic_favorite_solid)
+                }
+                true
+            }
+        }
+    }
+
+    private fun changeMenuIcon(item: MenuItem, icon: Int) = item.setIcon(icon)
 
 }
---
+++ app/src/main/java/com/example/foodium/viewmodel/RecipeDetailsViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.foodium.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.foodium.data.database.model.RecipeEntity
+import com.example.foodium.repository.AppRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RecipeDetailsViewModel @Inject constructor(private val repository: AppRepository) : ViewModel() {
+
+    fun insertRecipe(recipe: RecipeEntity) = viewModelScope.launch {
+        repository.insertRecipe(recipe)
+    }
+
+    fun deleteRecipe(recipe: RecipeEntity) = viewModelScope.launch {
+        repository.deleteRecipe(recipe)
+    }
+
+}
