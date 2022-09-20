e48b7383a19b23cde8e50088113938457ed1aa7b
YashKr01 <yashkumar201301@gmail.com>
2022-09-20 23:44:54 +0530

    Dao & Sort Order

--- app/src/main/java/com/techK/foodium/data/databse/dao/RecipeDao.kt
+++ app/src/main/java/com/techK/foodium/data/databse/dao/RecipeDao.kt
@@ -1,8 +1,40 @@
 package com.techK.foodium.data.databse.dao
 
-import androidx.room.Dao
+import androidx.room.*
+import com.techK.foodium.domain.entities.Recipe
+import com.techK.foodium.domain.enums.SortOrder
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RecipeDao {
+
+    @Query("SELECT * FROM recipes")
+    fun getRecipesListByOrder(sortOrder: SortOrder): Flow<List<Recipe>> =
+        when (sortOrder) {
+            SortOrder.BY_NAME -> sortByName()
+            SortOrder.BY_LIKES -> sortByLikes()
+            SortOrder.BY_TIME -> sortByTime()
+        }
+
+    @Query("SELECT * FROM recipes")
+    fun getRecipesList(): Flow<List<Recipe>>
+
+    @Query("SELECT * FROM recipes ORDER BY title ASC")
+    fun sortByName(): Flow<List<Recipe>>
+
+    @Query("SELECT * FROM recipes ORDER BY aggregateLikes DESC")
+    fun sortByLikes(): Flow<List<Recipe>>
+
+    @Query("SELECT * FROM recipes ORDER BY readyInMinutes ASC")
+    fun sortByTime(): Flow<List<Recipe>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipe(recipe: Recipe)
+
+    @Delete
+    suspend fun deleteRecipe(recipe: Recipe)
+
+    @Query("DELETE FROM recipes")
+    suspend fun deleteAllRecipes()
 
 }
